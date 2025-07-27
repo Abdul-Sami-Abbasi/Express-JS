bd60402074a63783ac28bb64be1f15c9376b2793
Abdul Sami <abdulsamiabbasi443@gmail.com>
2025-07-27 23:53:33 +0500

    files uploaded

--- index.js
+++ index.js
@@ -4,18 +4,26 @@
 //create instance express
 //provode port : 3000
 //basic route '/'
+//regex route in get
+// nested route in get
+// passing multiple call backs in get (through next parameter/function) and Array of Callbacks
 
-import express from 'express'
+import express from 'express';
+const app = express();
+const port = 3000;
 
-const app = express()
+const cb1 = (req, res, next)=>{
+    console.log('First Calllback!')
+    next();
+}
+const cb2 = (req, res, next)=>{
+    console.log('Second Calllback!')
+    next();
+}
+const cb3 = (req, res)=>{
+    res.send('<h1>Third Calllback!<h1/>')
+}
+ 
+app.get('/', [cb1, cb2, cb3])
 
-app.get('/', (req, res)=>{res.send('<h1>Home</h1>')})
-app.get('/about', (req,res)=>{res.send('<h1>About</h1>')})
-app.get('/contact', (req, res) =>{res.send('<h1>Contact us</h1>')})
-
-//regex route
-app.get(/^\/users\/[0-9]{4}$/, (req, res)=>{res.send('This will run if the path contain users/1234 or any 4 digit number')})
-
-//nested route
-app.get('/products/iphone', (req, res)=>{res.send('This will only run if the Route is products/iphone')})
-app.listen(3000, ()=>{console.log("Server Up babby!")})
+app.listen(port, ()=>{console.log("Server Up babby!")})
